6d0060ee9ddffcaf6cd9f75227ba8ca1d8296129
epond <epond@users.noreply.github.com>
2016-10-15 22:22:25 +0100

    add time ticker in main

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "github.com/epond/go-sketchpad/string"
+	"fmt"
+	"github.com/epond/go-sketchpad/string"
+	"time"
 )
 
 func main() {
-    fmt.Printf("Hello, %v\n", string.Reverse("World"))
+	fmt.Printf("Hello, %v\n", string.Reverse("World"))
+
+	i := 0
+	c := time.Tick(time.Duration(1) * time.Second)
+	fmt.Println("Doing work")
+	for _ = range c {
+		i += 1
+		// should we do work?
+		fmt.Println("Doing work")
+		// should we break from the loop?
+		if i > 5 {
+			break
+		}
+	}
+
 }
